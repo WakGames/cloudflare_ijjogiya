8eb1e691722c96de50550e5c44aa6fe67d109aec
Minsu Kim <minsu_kim@bishanoi.net>
2023-09-03 20:12:35 +0700

    :fire: feat: add github test and clean deps

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 const archiver = require('archiver')
 
 const { minify } = require('html-minifier-terser')
-const { minify: jsMinify } = require("terser");
 
 // create folders
 try {
@@ -10,8 +9,6 @@ try {
     fs.mkdirSync('./dist')
     fs.mkdirSync('./dist/assets')
     fs.mkdirSync('./dist/assets/css')
-    fs.mkdirSync('./dist/assets/img')
-    fs.mkdirSync('./dist/assets/js')
 } catch (e) {
     if (e.errno !== -4075) {
         throw e
@@ -54,24 +51,6 @@ fs.readdirSync('./src/assets/css').forEach(async file => {
 
         fs.writeFileSync(`./dist/assets/css/${file}`, minified)
     }
-})
-
-fs.readdirSync('./src/assets/js').forEach(async file => {
-    if (file.endsWith('.js')) {
-        const js = fs.readFileSync(`./src/assets/js/${file}`, 'utf8')
-
-        const minified = await jsMinify(js, {
-            sourceMap: true
-        })
-
-        fs.writeFileSync(`./dist/assets/js/${file}`, minified.code)
-        fs.writeFileSync(`./dist/assets/js/${file}.map`, minified.map)
-    }
-})
-
-// copy images
-fs.readdirSync('./src/assets/img').forEach(file => {
-    fs.copyFileSync(`./src/assets/img/${file}`, `./dist/assets/img/${file}`)
 })
 
 // zip dist file
